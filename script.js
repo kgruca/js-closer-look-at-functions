9f7a32e396fd5dcc0e7136d2fcf8867f74785d58
Krzysztof Gruca <kgruca21@gmail.com>
2022-09-11 14:59:32 -0700

    default params part 1

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+const bookingArr = [];
+
+
+const createBooking = function (flightNum, numPassengers, price) {
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookingArr.push(bookingArr);
+};
+
+createBooking('LOT2107', 321, 1356);
