d323f574d13e45583fe963c4bade1c70fc624ad3
Krzysztof Gruca <kgruca21@gmail.com>
2022-09-12 22:56:00 -0700

    funtion default params ES5 vs ES6

--- script.js
+++ script.js
@@ -4,7 +4,14 @@
 const bookingArr = [];
 
 
-const createBooking = function (flightNum, numPassengers, price) {
+const createBooking = function (flightNum, numPassengers = 5, price = 99) {
+    // the following is the ES5 way of assigning default values
+    // numPassengers = numPassengers || 1;
+    // price = price || 99;
+
+    // since ES6, the default params can be added directly to the list of 
+    // parameters in the functions definition (see above)
+    
     const booking = {
         flightNum,
         numPassengers,
